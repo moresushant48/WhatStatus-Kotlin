117dabd92005f797878b179b559f5cb2cbd290df
Sushant More <moresushant48@gmail.com>
2019-12-05 19:08:48 +0530

    Permissions : Request READ_EXTERNAL_STORAGE.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- app/src/main/java/io/moresushant48/whatstatus/MainActivity.kt
+++ app/src/main/java/io/moresushant48/whatstatus/MainActivity.kt
@@ -4,12 +4,18 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private val STORAGE_PERMISSION_REQUEST_CODE: Int = 1000
     private lateinit var toolbar: Toolbar
     private lateinit var recyclerView: RecyclerView
     private lateinit var fileRefreshLayout: SwipeRefreshLayout
@@ -29,11 +35,21 @@ class MainActivity : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
 
-        startApp()
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(this, "Please allow Storage Permission.", Toast.LENGTH_LONG).show()
+            }else askForPermission()
+        }else{
+            startApp()
+        }
 
         fileRefreshLayout.setOnRefreshListener {
             startApp()
         }
+    }
+
+    private fun askForPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_REQUEST_CODE)
     }
 
     private fun startApp() {
@@ -46,5 +62,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         fileRefreshLayout.isRefreshing = false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+
+            STORAGE_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    startApp()
+                else {
+                    Toast.makeText(this, "App requires storage access.", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            askForPermission()
+        }else startApp()
     }
 }
