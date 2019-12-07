7021b3aed6b557344e62a79f895169294240d93a
Sushant More <moresushant48@gmail.com>
2019-12-07 19:54:31 +0530

    ViewStatus : Create func to return file type, boolean
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- app/src/main/java/io/moresushant48/whatstatus/ViewStatus.kt
+++ app/src/main/java/io/moresushant48/whatstatus/ViewStatus.kt
@@ -1,14 +1,19 @@
 package io.moresushant48.whatstatus
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
 import android.widget.MediaController
 import android.widget.VideoView
+import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_view_status.*
+import java.io.File
 
 class ViewStatus : AppCompatActivity() {
 
@@ -17,6 +22,8 @@ class ViewStatus : AppCompatActivity() {
 
     private lateinit var statusImage: ImageView
     private lateinit var statusVideo: VideoView
+
+    private lateinit var fileUriString: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,25 +36,25 @@ class ViewStatus : AppCompatActivity() {
         statusImage = findViewById(R.id.statusImage)
         statusVideo = findViewById(R.id.statusVideo)
 
-        val uri = intent.getStringExtra("uri")
+        fileUriString = intent.getStringExtra("uri")
 
         val mediaController = MediaController(this)
 
-        if (intent.getStringExtra("uri").endsWith(".jpg")) {
+        if (isStatusAnImage()) {
             statusImage.visibility = View.VISIBLE
             Glide.with(this@ViewStatus)
-                .load(uri)
+                .load(fileUriString)
                 .into(statusImage)
         } else {
 
             statusImage.visibility = View.VISIBLE
             Glide.with(this@ViewStatus)
-                .load(uri)
+                .load(fileUriString)
                 .into(statusImage)
 
             statusVideoPlaySign.visibility = View.VISIBLE
             statusVideo.visibility = View.VISIBLE
-            statusVideo.setVideoURI(Uri.parse(uri))
+            statusVideo.setVideoURI(Uri.parse(fileUriString))
 
             statusVideo.setMediaController(mediaController)
             mediaController.setAnchorView(statusVideo)
@@ -63,6 +70,11 @@ class ViewStatus : AppCompatActivity() {
             imgStatusVideoPlaySign.visibility = View.VISIBLE
             statusImage.visibility = View.VISIBLE
         }
+    }
+
+    fun isStatusAnImage(): Boolean {
+
+        return fileUriString.endsWith(".jpg")
     }
 
     override fun onBackPressed() {
